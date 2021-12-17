eda8bc21628603baf2a89be81d85eb0ddc6f4653
DECHACHETaousCHPS <34868719+DECHACHETaousCHPS@users.noreply.github.com>
2021-12-17 07:36:27 +0100

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class multiplication {
+
+   public static void main( String[] args ) {
+      int a=41, b=23, c = 1;
+      c = a * b;
+      System.out.println( "Le résultat de la multiplication entre  a et b est: " + c );
+   }
+
+}
---
+++ somme.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class somme {
+
+   public static void main( String[] args ) {
+      int a=100, b=200, c = 0;
+      
+      c = a + b;
+      System.out.println( "Le résultat de l'addition entre  a et b est: " + c );
+   }
+
+}
